95b7c4a5376dbb257895a7e8908579ccb4f094e5
Yuanpeng Cao <yuanpenc@YuanpengdeMacBook-Pro.local>
2020-11-04 15:26:32 +0800

    Add profile, muinfo, cart html. Add new paths to demo

--- eCommerce/urls.py
+++ eCommerce/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('', views.stream_action, name='home'),
     path('register', views.register_action, name='register'),
     path('login', views.login_action, name='login'),
+    path('item_detail', views.detail, name='item_detail'),
+    path('list_items', views.list, name='list_items'),
+    path('profile', views.profile, name='profile'),
+    path('myinfo', views.myinfo, name='myinfo'),
+    path('cart', views.cart, name='cart'),
 ]
