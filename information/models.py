b7e47bc1fb895ef6699c97c1147486da62f3f679
Yuanpeng Cao <yuanpenc@YuanpengdeMacBook-Pro.local>
2020-11-29 19:35:38 +0800

    Peng check the cart html and cart css

--- information/models.py
+++ information/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from goods.models import Items
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,
+                                related_name="profile",
+                                on_delete=models.CASCADE)
+
+    picture = models.FileField(blank=True)
+    content_type = models.CharField(max_length=50)
+    address = models.CharField(max_length=200, default="Greenfield 829 Ave, Pittsburgh, PA")
+    birthday = models.CharField(max_length=200, default="01-01-2020")
+    gender = models.CharField(max_length=200, default="male")
+
+
+    def __unicode__(self):
+        return 'id=' + str(self.id) + ',bio="' + self.bio + '"'
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, related_name="cart",on_delete=models.CASCADE)
+    goods = models.ForeignKey(Items,on_delete=models.CASCADE)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
--- information/urls.py
+++ information/urls.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
 from django.urls import path
-from eCommerce import views
+from information import views
+from goods import views as g_views
 
 urlpatterns = [
-    path('', views.stream_action, name='home'),
+    path('', views.myinfo, name='home'),
     path('register', views.register_action, name='register'),
     path('login', views.login_action, name='login'),
     # path('item_detail', views.detail, name='item_detail'),
     # path('list_items', views.list, name='list_items'),
-    path('profile', views.profile, name='profile'),
-    path('myinfo', views.myinfo, name='myinfo'),
-    path('cart', views.cart, name='cart'),
+    # path('profile', views.profile, name='profile'),
+    # path('myinfo', views.myinfo, name='myinfo'),
+    path('cart', views.cart_page, name='cart'),
+    path('photo/<int:id>', g_views.get_photo, name='photo'),
     # This path is for demo
-    path('nav_list', views.stream_action, name='nav_list')
+    # path('nav_list', views.stream_action, name='nav_list')
 ]
