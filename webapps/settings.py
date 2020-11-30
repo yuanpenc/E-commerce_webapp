4c08f2f44f6e59ef26628a1e452852420be555e0
Peng Zeng <bhxxzp@gmail.com>
2020-11-30 02:14:54 -0500

    Final Version

--- webapps/settings.py
+++ webapps/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'eCommerce',
     'goods',
     'information',
     'seller',
